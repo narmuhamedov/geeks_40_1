bcf18d0f80ef1532c75965cd06186c98dbfce7c3
rzpy1997 <kingradomir@gmail.com>
2024-05-21 18:25:45 +0600

    'lesson5'

--- telefone/urls.py
+++ telefone/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('phone_list/', views.phone_list, name='phone_list'),
-    path('phone_list/<int:id>/', views.phone_detail, name='phone_detail'),
+    path('phone_list/', views.PhoneListView.as_view(), name='phone_list'),
+    path('phone_list/<int:id>/', views.PhoneDetailView.as_view(), name='phone_detail'),
+    path('phone_list/<int:id>/delete/', views.DeletePhoneView.as_view(), name='delete_phone'),
+    path('phone_list/<int:id>/update/', views.EditPhoneView.as_view(), name='update_phone'),
+    path('create_phone/', views.CreatePhoneView.as_view(), name='create_phone'),
+    path('search/', views.SearchPhoneView.as_view(), name='search'),
 ]
---
+++ test.py
@@ -0,0 +1,3 @@
+data_tuple = ('qwer', 'erw', 12, True)
+data_tuple1 = list(data_tuple)
+print(data_tuple1)
